96d2d2175c9f9417ee3e27cde68337954bffd185
Sherief Elsowiny <elsowiny@yahoo.com>
2021-12-14 18:21:58 -0500

    work on frontend part

--- adminclient/admin/src/App.tsx
+++ adminclient/admin/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate, BrowserRouter} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={"/"}>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
