3ee6e7d756f49407da3ec558fa89936b13ffe9d9
elsowiny <elsowiny@yahoo.com>
2021-12-14 21:58:08 -0500

    setting up connections between both apps

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,38 @@ import initAndFill from './database/initAndFill';
 import initPassportAndSessions from './passport/passport.sessions.config';
 import routerInit from './routes/router';
 import flash from 'connect-flash';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 
 initAndFill();
 
-var cors = require('cors');
 
+/*
+const whiteList = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
+
+
+const corsOptions: cors.CorsOptions = {
+      origin: function (origin, callback) {
+            if (whiteList.indexOf(origin) !== -1) {
+                  callback(null, true);
+            } else {
+                  callback(new Error('Not allowed by CORS'));
+            }
+      }
+};
+*/
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 9010);
 
 initPassportAndSessions(app);
 
-app.use(cors())
+
 
 
 app.get('/', (req, res) => {
