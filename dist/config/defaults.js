db77c98c59e2609a008043a77fe7770573bd7d03
Sherief Elsowiny <elsowiny@yahoo.com>
2021-12-16 23:04:24 -0500

    removed dist

--- dist/config/defaults.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    "mongoURI": "mongodb://localhost:27017/mongo-sess-jajamaru",
-    "sessionSecret": "jajamaru-secret",
-    "adminClient": "localhost:12000",
-};
-exports.default = config;
-//# sourceMappingURL=defaults.js.map
